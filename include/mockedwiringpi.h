173c948e9001e38bd4c469a162ae922b2d0fa106
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-12-17 11:08:23 +0100

    ADDED inline keyword in the mocked WiringPi functions

--- include/mockedwiringpi.h
+++ include/mockedwiringpi.h
@@ -24,26 +24,26 @@ namespace  nunchuckadapter {
 
     MockedWiringPiI2C mockedI2C = {42};
 
-    void wiringPiSetup() {
+    inline void wiringPiSetup() {
         std::cout << "Set up function of the WiringPi Library Called" << std::endl;
     }
 
-    int wiringPiI2CSetup(int deviceAddress) {
+    inline int wiringPiI2CSetup(int deviceAddress) {
         return mockedI2C.i2CFileDescriptor;
     }
 
-    void wiringPiI2CWriteReg8(int i2CPortFileDescriptor, int deviceAddress, int data) {
+    inline void wiringPiI2CWriteReg8(int i2CPortFileDescriptor, int deviceAddress, int data) {
         std::cout <<
                   "Write on i2c port > Device Address " << deviceAddress <<
                   " Data: " << data
                   << std::endl;
     }
 
-    void wiringPiI2CWrite(int i2CPortFileDescriptor, int data) {
+    inline void wiringPiI2CWrite(int i2CPortFileDescriptor, int data) {
         //std::cout << "Write on i2c port > Data: " << data << std::endl;
     }
 
-    int wiringPiI2CRead(int i2CPortFileDescriptor) {
+    inline int wiringPiI2CRead(int i2CPortFileDescriptor) {
         return rand() % 256;
     }
 }
